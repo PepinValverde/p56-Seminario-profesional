2531f222e2b02d5cf205fe2d34e5391eb574d4df
Jose Valverde <fvalverde@est.ups.edu.ec>
2020-09-29 20:31:53 -0500

    Se subio a master para luego respaldar 20 :32 / 29 sep

--- Code/javascript/11_clases_jp.js
+++ Code/javascript/11_clases_jp.js
@@ -1,19 +1,19 @@
 // Java Scrpt no es lenguaje orientado a Objetos
 // en Js no se puede hacer herecia pero se puede emular de la siguiente forma
 function heredaDe (prototipoHijo, prototipoPadre){ // se hace en una variale temporal de tipo funcion, asi se hereda de otra Clase
-    var fn = function(){} // declara funcion vacia
+    var fn = function(){} // declara funcion vacia, a esto se llama sobreescritura
     fn.prototype = prototipoPadre.prototype  // aqui le doy los valores de Clase Padre
 // aqui obtengo los atribitos y metodos de Padre
     prototipoHijo.prototype = new fn // // lo q se hace es instansiar en prototipoPadre
     prototipoHijo.prototype.constructor = prototipoHijo // se le pasan todos los valores de prototipoHIjpo pero en constructor
 }
 
-function  Persona (nombre, apellido, altuta){ // clase padre
+function  Persona (nombre, apellido, altuta){ // clase padre (constructor)
 this.nombre = nombre
 this.apellido = apellido
 this.altuta = altuta
 }
-funcion Deportista (nombre, apellido){
+funcion Deportista (nombre, apellido) {
     this.nombre = nombre
     this.apellido = apellido
 }
