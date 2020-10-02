4c2f97fdb7026d73f4cc9580a25c0d85842a151f
Jose Valverde R <fvalverde@est.ups.edu.ec>
2020-10-02 17:28:27 -0500

    prueba de index

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const db = require ('mongoose') 
+const db = require('mongoose')
 
 db.Promise = global.Promise
 
-async function connect(uri){
-await db.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'utups',
-})
-    .then((data) => {console.log('[db] - Conectada con exito')})
-    .catch((error) => {console.error('[error log] - ' + error)})
+async function connect(uri) {
+    await db.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'utups',
+    })
+        .then((data) => { console.log('[db] - Conectado Compadre.') })
+        .catch((error) => {  console.error( '[error log] - ' + error ) })    
 }
 
-module.exports= connect
+module.exports = connect
