81351cac51fb5e29ae8a6426c17bd1a009ef8030
Jose Valverde R <fvalverde@est.ups.edu.ec>
2020-10-02 22:47:44 -0500

    Ahora si se carhga a Heroku, de ley

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.pjyad.gcp.mongodb.net/utups?retryWrites=true&w=majority',
-    port: process.env.PORT || 5000,
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.nwiwp.gcp.mongodb.net/utups?retryWrites=true&w=majority',
+    port: process.env.PORT || 5000,// mongodb+srv://ups:<password>@cluster0.nwiwp.gcp.mongodb.net/test
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
-    // filesRoute: process.env.FILES_ROUTE || 'files',
-}
+    filesRoute: process.env.FILES_ROUTE || 'files',
+}                                   
 
 module.exports = config
