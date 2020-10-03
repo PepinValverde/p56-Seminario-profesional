8a74d2094d60dfe0cf9f6e1042cea07427147f06
Jose Valverde R <fvalverde@est.ups.edu.ec>
2020-10-02 22:07:15 -0500

    Ultima modificacion

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
-    filesRoute: process.env.FILES_ROUTE || 'files',
+    // filesRoute: process.env.FILES_ROUTE || 'files',
 }
 
 module.exports = config
